5d46a6105b031c15e8decec89ba39b4dcb7d02f3
Felix Fang <felixf@qualtrics.com>
2018-06-23 11:44:09 -0700

    Compute readability scores

--- consolidate_output.py
+++ consolidate_output.py
@@ -4,6 +4,24 @@ import glob
 import json
 import os
 from collections import defaultdict, OrderedDict
+from typing import List
+
+import numpy as np
+import spacy
+from textacy.text_stats import TextStats
+
+spacy_pipe = spacy.load('en_core_web_sm', disable=['parser', 'ner', 'tagger'])
+spacy_pipe.add_pipe(spacy_pipe.create_pipe('sentencizer'))
+
+
+def get_scores(texts: List[str]):
+    scores = [TextStats(doc).readability_stats for doc in spacy_pipe.pipe(texts, batch_size=1024)]
+    scores = list(filter(None, scores))
+
+    return {
+        key: np.mean(list(s[key] for s in scores))
+        for key in scores[0].keys()
+    }
 
 
 def main():
@@ -15,17 +33,23 @@ def main():
     args = parser.parse_args()
 
     results = defaultdict(OrderedDict)
+    articles = {}
     for data_filepath in glob.iglob(os.path.join(args.data_dir, args.split, '*.json'), recursive=False):
         data_filename = os.path.basename(os.path.normpath(data_filepath))
         name, ext = os.path.splitext(data_filename)
-        results[int(name)]['article'] = '\n'.join(json.load(open(data_filepath, 'r', encoding='utf8'))['article'])
+        index = int(name)
+        articles[index] = json.load(open(data_filepath, 'r', encoding='utf8'))['article']
+        results[index]['article'] = '\n'.join(articles[index])
 
+    summaries = defaultdict(OrderedDict)
     for model_folder in sorted(list(glob.iglob(os.path.join(args.decode_dir, '*'), recursive=False))):
         model_name = os.path.basename(os.path.normpath(model_folder))
-        for dec_filepath in glob.iglob(os.path.join(model_folder, 'output', '*.dec'), recursive=False):
+        for dec_filepath in glob.iglob(os.path.join(model_folder, args.split, 'output', '*.dec'), recursive=False):
             dec_filename = os.path.basename(os.path.normpath(dec_filepath))
             name, ext = os.path.splitext(dec_filename)
-            results[int(name)][model_name] = ''.join(open(dec_filepath, 'r', encoding='utf8')).strip()
+            index = int(name)
+            summaries[index][model_name] = [line.strip() for line in open(dec_filepath, 'r', encoding='utf8')]
+            results[index][model_name] = '\n'.join(summaries[index][model_name])
 
     fieldnames = ['id'] + list(results[0].keys())
     with open(args.output_file, 'w', encoding='utf8') as out:
@@ -33,6 +57,31 @@ def main():
         csv_writer.writeheader()
         for key in sorted(list(results.keys())):
             csv_writer.writerow({'id': key, **results[key]})
+
+    output_file_name, _ = os.path.splitext(args.output_file)
+
+    json_results = []
+    for index in sorted(list(articles.keys())):
+        json_results.append(OrderedDict([
+            ('id', index),
+            ('article', articles[index]),
+            ('summaries', summaries[index]),
+            ('readability_scores', {model: get_scores(texts) for model, texts in summaries[index].items()}),
+        ]))
+    with open(os.path.normpath(output_file_name) + '.json', 'w', encoding='utf8') as out:
+        json.dump(json_results, out, indent=4, ensure_ascii=False)
+
+    scores_results = []
+    for model in summaries[0].keys():
+        scores = [j['readability_scores'][model] for j in json_results]
+        scores_results.append(OrderedDict(
+            [('model', model)] +
+            [(key, np.mean(list(s[key] for s in scores))) for key in scores[0].keys()]))
+
+    with open(os.path.normpath(output_file_name) + '_readability_scores.csv', 'w', encoding='utf8') as out:
+        csv_writer = csv.DictWriter(out, scores_results[0].keys(), quoting=csv.QUOTE_NONNUMERIC)
+        csv_writer.writeheader()
+        csv_writer.writerows(scores_results)
 
 
 if __name__ == '__main__':
