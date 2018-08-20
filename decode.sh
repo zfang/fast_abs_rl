341181e1c09625749b24910468d5dc863d112e1b
Felix Fang <felixf@qualtrics.com>
2018-08-19 22:26:50 -0700

    Update script

--- decode.sh
+++ decode.sh
@@ -1,3 +1,5 @@
 MODEL=$1
-MODE=$2
-python decode_full_model.py --path=.output_decode/$MODEL/$MODE --model_dir=pretrained/$MODEL --beam=5 --$MODE
+shift
+MODE=$1
+shift
+python decode_full_model.py --path=.output_decode/$MODEL/$MODE --model_dir=pretrained/$MODEL --beam=5 --$MODE $@
